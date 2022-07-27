8a47afdc556e7baa61ab7e7ba5e6d845c66499e8
pboymt <10893582+pboymt@users.noreply.github.com>
2022-07-27 23:20:43 +0800

    build: add environment variable checking of opencv

--- check-opencv.js
+++ check-opencv.js
@@ -6,31 +6,45 @@ const keys = ['disableAutoBuild', 'opencvIncludeDir', 'opencvLibDir', 'opencvBin
 
 console.log('Checking OpenCV...');
 
-if (package.opencv4nodejs) {
-    const section = package.opencv4nodejs;
-    for (const key of keys) {
-        if (key === 'disableAutoBuild') {
-            if (typeof section[key] === 'number') {
-                if (section[key] !== 1) {
+function checkPackageJson() {
+    if (package.opencv4nodejs) {
+        const section = package.opencv4nodejs;
+        for (const key of keys) {
+            if (key === 'disableAutoBuild') {
+                if (typeof section[key] === 'number') {
+                    if (section[key] !== 1) {
+                        break;
+                    }
+                } else {
                     break;
                 }
             } else {
-                break;
-            }
-        } else {
-            if (typeof section[key] !== 'string') {
-                break;
-            }
-            try {
-                const stat = fs.statSync(section[key]);
-                if (!stat.isDirectory()) {
+                if (typeof section[key] !== 'string') {
+                    break;
+                }
+                try {
+                    const stat = fs.statSync(section[key]);
+                    if (!stat.isDirectory()) {
+                        throw new Error(`${key} is not a directory: ${section[key]}`);
+                    }
+                } catch (error) {
                     throw new Error(`${key} is not a directory: ${section[key]}`);
                 }
-            } catch (error) {
-                throw new Error(`${key} is not a directory: ${section[key]}`);
             }
         }
     }
+}
+
+if (process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD === '1') {
+    if (process.env.OPENCV_INCLUDE_DIR && process.env.OPENCV_LIB_DIR && process.env.OPENCV_BIN_DIR) {
+        console.log('OpenCV has been detected as installed manually.');
+    } else {
+        console.log('OPENCV4NODEJS_DISABLE_AUTOBUILD is set to 1, but environment variables of OpenCV are not been detected as installed manually.');
+        checkPackageJson();
+    }
+} else {
+    console.log('Checking package.json...');
+    checkPackageJson();
 }
 
 console.log('OK');
