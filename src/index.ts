4d08635f6bd7ee78b0206e4f112fe277081c4dde
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-01 19:20:37 +0800

    feat: use commander to handle command and options

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,52 @@
 // import cv from '@u4/opencv4nodejs';
-import { createClient } from '@u4/adbkit';
 import { writeFile } from 'node:fs/promises';
-import { Device } from './core/adb/device';
+import { createClient, Device } from './core/adb';
+import { program } from 'commander';
+import { join } from 'node:path';
+import moment from 'moment';
 
-async function main() {
-    // const img = await cv.imreadAsync('./test.jpg');
-    // console.log(img.sizes);
-    // await cv.imshowWait('test', img);
-    const adb = createClient();
-    const list = await adb.listDevices();
-    const { id, type } = list[0];
-    const device = new Device(adb, id);
-    const data = await device.screenshot();
-    // const data = await loadDataFromStream(stream);
-    await writeFile('./src.jpg', data);
+program
+    .name('wfa')
+    .version('0.0.1')
+    .description('WFNodejs is a tool for Android device to play World Flipper automatically.');
+
+interface CShotOptions {
+    device?: string;
+    saveDir: string;
 }
 
-main();
+program
+    .command('shot [filename]')
+    .option('-d, --device <id>', 'device id')
+    .option('-s, --save-dir <dir>', 'save directory', join(process.cwd(), 'screenshot'))
+    .description('Take a screenshot and save it to a file.')
+    .action(async (filename: string | undefined, options: CShotOptions) => {
+        const adb = await createClient();
+        const list = await adb.listDevices();
+        if (list.filter(d => d.type === 'device').length === 0) {
+            console.error('No device connected.');
+            return;
+        } else {
+            try {
+                let device: Device;
+                if (options.device) {
+                    const result = list.find(d => d.id === options.device);
+                    if (result) {
+                        device = adb.getDevice(result.id);
+                    } else {
+                        console.error('Device not found.');
+                        return;
+                    }
+                } else {
+                    device = adb.getDevice(list[0].id);
+                }
+                const filepath = join(options.saveDir, filename ? `${filename}.png` : `${moment().format('YYYYMMDD-HHmmss')}.png`);
+                const img = await device.screenshot();
+                await writeFile(filepath, img);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    });
+
+program.parse();
