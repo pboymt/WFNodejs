811aa4d68cfa104c8594d30bb4efa6901ea1d42d
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-04 22:42:58 +0800

    feat: 添加 OpenCV 的自动配置脚本

---
+++ postinstall.js
@@ -0,0 +1,94 @@
+const dotenv = require('dotenv');
+const { spawnSync } = require('node:child_process');
+const { existsSync } = require('node:fs');
+const fs = require('node:fs');
+const { writeFile } = require('node:fs/promises');
+const { join, resolve } = require('node:path');
+const path = require('node:path');
+const package = require('./package.json');
+
+dotenv.config();
+
+const keys = ['disableAutoBuild', 'opencvIncludeDir', 'opencvLibDir', 'opencvBinDir'];
+
+console.log('Checking OpenCV...');
+
+function checkPackageJson() {
+    if (package.opencv4nodejs) {
+        const section = package.opencv4nodejs;
+        for (const key of keys) {
+            if (key === 'disableAutoBuild') {
+                if (typeof section[key] === 'number') {
+                    if (section[key] !== 1) {
+                        break;
+                    }
+                    process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD = '1';
+                } else {
+                    break;
+                }
+            } else {
+                if (typeof section[key] !== 'string') {
+                    break;
+                }
+                try {
+                    const stat = fs.statSync(section[key]);
+                    if (!stat.isDirectory()) {
+                        throw new Error(`${key} is not a directory: ${section[key]}`);
+                    } else {
+                        switch (key) {
+                            case 'opencvIncludeDir':
+                                process.env.OPENCV_INCLUDE_DIR = resolve(section[key]);
+                                break;
+                            case 'opencvLibDir':
+                                process.env.OPENCV_LIB_DIR = resolve(section[key]);
+                                break;
+                            case 'opencvBinDir':
+                                process.env.OPENCV_BIN_DIR = resolve(section[key]);
+                                break;
+                        }
+                    }
+                } catch (error) {
+                    throw new Error(`${key} is not a directory: ${section[key]}`);
+                }
+            }
+        }
+    }
+}
+
+if (process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD === '1') {
+    if (process.env.OPENCV_INCLUDE_DIR && process.env.OPENCV_LIB_DIR && process.env.OPENCV_BIN_DIR) {
+        console.log('发现环境变量中的 OpenCV 目录，不需要单独构建');
+    } else {
+        console.log('环境变量 OPENCV4NODEJS_DISABLE_AUTOBUILD 被设置为 1，但环境变量中的 OPENCV_INCLUDE_DIR、OPENCV_LIB_DIR、OPENCV_BIN_DIR 不存在，将自动构建 OpenCV');
+        process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD = '0';
+    }
+}
+
+(async () => {
+
+    try {
+
+        if (existsSync('node_modules/@u4/opencv4nodejs/build/Release/opencv4nodejs.node')) {
+            console.log('opencv4nodejs.node 已被编译过，不需要重新编译');
+            process.exit(0);
+        }
+
+        const cp = spawnSync('npm.cmd', ['exec', 'build-opencv', 'rebuild'], {
+            stdio: 'inherit', env: process.env
+        });
+
+        if (cp.status !== 0) {
+            throw new Error('OpenCV 构建失败');
+        }
+
+        console.log('OpenCV 构建成功');
+
+    } catch (error) {
+
+        console.error(error);
+        process.exit(1);
+
+    }
+
+})();
+
---
+++ preinstall.js
@@ -0,0 +1,202 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+async function checkPlatform() {
+    const platform = process.platform;
+    if (platform !== 'win32') {
+        throw new Error(`不支持的平台：${platform}`);
+    }
+    return platform;
+}
+
+async function keypress() {
+    process.stdin.setRawMode(true);
+    return new Promise(resolve => process.stdin.once('data', data => {
+        const byteArray = [...data];
+        if (byteArray.length > 0 && byteArray[0] === 3) {
+            console.log('cancel');
+            process.exit(1);
+        }
+        process.stdin.setRawMode(false);
+        resolve(byteArray[0]);
+    }))
+}
+
+// download from url with handle http 302 redirect
+async function downloadWithRedirect(url, dest) {
+    return new Promise((resolve, reject) => {
+        const req = https.get(url, async res => {
+            if (res.statusCode === 302) {
+                const location = res.headers.location;
+                if (location) {
+                    await downloadWithRedirect(location, dest).then(resolve).catch(reject);
+                } else {
+                    reject(new Error('302 redirect but no location header'));
+                }
+            } else if (res.statusCode === 200) {
+                const total = parseInt(res.headers['content-length'], 10);
+                let downloaded = 0;
+                const file = fs.createWriteStream(dest);
+                // show progress
+                res.on('data', chunk => {
+                    // clean this line
+                    downloaded += chunk.length;
+                    process.stdout.clearLine();
+                    // show downloaded size / total size and download percentage
+                    process.stdout.write(`\r${Math.round(downloaded / 1024)}KB/${Math.round(total / 1024)}KB (${Math.round(downloaded / total * 100)}%)`);
+                });
+                res.pipe(file);
+                file.on('finish', () => {
+                    file.close();
+                    resolve();
+                });
+            } else {
+                reject(new Error(`status code ${res.statusCode}`));
+            }
+        });
+        req.on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+const pathList = {
+    'opencv': {
+        OPENCV_INCLUDE_DIR: 'opencv/build/include',
+        OPENCV_LIB_DIR: 'opencv/build//x64/vc15/lib',
+        OPENCV_BIN_DIR: 'opencv/build//x64/vc15/bin'
+    },
+    'opencv/opencv': {
+        OPENCV_INCLUDE_DIR: 'opencv/opencv/build/include',
+        OPENCV_LIB_DIR: 'opencv/opencv/build/x64/vc15/lib',
+        OPENCV_BIN_DIR: 'opencv/opencv/build/x64/vc15/bin',
+    },
+    'env': {
+        OPENCV_INCLUDE_DIR: process.env.OPENCV_INCLUDE_DIR,
+        OPENCV_LIB_DIR: process.env.OPENCV_LIB_DIR,
+        OPENCV_BIN_DIR: process.env.OPENCV_BIN_DIR,
+    }
+}
+
+async function checkOpenCV() {
+    console.log('检查环境变量中的OpenCV目录');
+    let exists = false;
+    const env = pathList.env;
+    for (const [k, p] of Object.entries(env)) {
+        if (!p) {
+            console.log(`缺失环境变量 ${k}`);
+            exists = false;
+            break;
+        } else {
+            if (fs.existsSync(p)) {
+                const fstat = fs.statSync(p);
+                if (fstat.isDirectory()) {
+                    exists = true;
+                } else {
+                    console.log(`环境变量 ${k} 设置的目录 ${p} 不是目录`);
+                    exists = false;
+                    break;
+                }
+            }
+        }
+    }
+    if (exists) {
+        console.log('环境变量中的 OpenCV 目录已存在，不需要下载');
+        return true;
+    }
+    console.log('检查 opencv 目录');
+    const opencv = pathList.opencv;
+    for (const p of opencv) {
+        if (fs.existsSync(p)) {
+            const fstat = fs.statSync(p);
+            if (fstat.isDirectory()) {
+                exists = true;
+            } else {
+                console.log(`准备提供给环境变量 ${k} 的路径 ${p} 不是目录`);
+                exists = false;
+                break;
+            }
+        } else {
+            console.log(`准备提供给环境变量 ${k} 的路径 ${p} 不存在`);
+            exists = false;
+            break;
+        }
+    }
+    if (exists) {
+        console.log('OpenCV 目录已存在，不需要下载');
+        process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD = '1';
+        process.env.OPENCV_INCLUDE_DIR = opencv.OPENCV_INCLUDE_DIR;
+        process.env.OPENCV_LIB_DIR = opencv.OPENCV_LIB_DIR;
+        process.env.OPENCV_BIN_DIR = opencv.OPENCV_BIN_DIR;
+        return true;
+    }
+    console.log('检查 opencv/opencv 目录');
+    const opencvOpencv = pathList['opencv/opencv'];
+    for (const p of opencvOpencv) {
+        if (fs.existsSync(p)) {
+            const fstat = fs.statSync(p);
+            if (fstat.isDirectory()) {
+                exists = true;
+            } else {
+                console.log(`准备提供给环境变量 ${k} 的路径 ${p} 不是目录`);
+                exists = false;
+                break;
+            }
+        } else {
+            console.log(`准备提供给环境变量 ${k} 的路径 ${p} 不存在`);
+            exists = false;
+            break;
+        }
+    }
+    if (exists) {
+        console.log('OpenCV 目录已存在，不需要下载');
+        process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD = '1';
+        process.env.OPENCV_INCLUDE_DIR = opencvOpencv.OPENCV_INCLUDE_DIR;
+        process.env.OPENCV_LIB_DIR = opencvOpencv.OPENCV_LIB_DIR;
+        process.env.OPENCV_BIN_DIR = opencvOpencv.OPENCV_BIN_DIR;
+        return true;
+    } else {
+        console.log('OpenCV 目录不存在，需要下载');
+        return false;
+    }
+}
+
+
+(async () => {
+
+    checkPlatform();
+
+    if (process.env.OPENCV4NODEJS_DISABLE_AUTOBUILD !== '1') {
+        console.log('自动构建 OpenCV，不再检查是否配置了 OpenCV');
+        process.exit(0);
+    }
+
+    const opencv_is_exists = await checkOpenCV();
+    if (opencv_is_exists) return;
+
+    console.log('是否下载安装 OpenCV？（Yy/Nn，默认为N）');
+    const key1 = await keypress();
+    if ([89, 121].includes(key1)) {
+        console.log('下载安装 OpenCV...');
+        await downloadWithRedirect('https://github.com/opencv/opencv/releases/download/4.6.0/opencv-4.6.0-vc14_vc15.exe', 'opencv/opencv.exe');
+        console.log('下载完成！');
+        spawnSync('./opencv/opencv.exe', ['-o', './opencv/', '-y']);
+    } else {
+        console.log('您选择了不下载安装 OpenCV！你需要等候自动编译！如果您想自行配置 OpenCV，请按 Ctrl-C 停止安装进程，请阅读 README.md 查看配置方法');
+    }
+
+    console.log('配置环境变量 .env');
+    const dotenv_content = `OPENCV4NODEJS_DISABLE_AUTOBUILD=1\n` +
+        `OPENCV_INCLUDE_DIR=${process.env.OPENCV_INCLUDE_DIR}\n` +
+        `OPENCV_LIB_DIR=${process.env.OPENCV_LIB_DIR}\n` +
+        `OPENCV_BIN_DIR=${process.env.OPENCV_BIN_DIR}\n`;
+    await writeFile(join(__dirname, '.env'), dotenv_content, 'utf8');
+
+})().then(() => {
+    process.exit();
+}).catch(e => {
+    console.error(e);
+    process.exit(1);
+});
