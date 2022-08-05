653be6d82985b03a8f0b877322f65e9c43792eb5
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-05 16:52:52 +0800

    feat: 添加图片模板加载流程，补充脚本执行流程

--- src/core/script/base.ts
+++ src/core/script/base.ts
@@ -1,14 +1,32 @@
+import { getLogger, Logger } from "log4js";
 import { setTimeout } from "timers/promises";
 import { Device } from "../adb";
+import { Target } from "../image/target";
 import { logger } from "../utils/logger";
 
-export abstract class BaseScript {
+export abstract class BaseScript<L extends string> {
 
+    abstract script_name: string;
     abstract package_name: string;
+    abstract target_list: Set<L>;
     private device: Device;
+    private map_target = new Map<L, Target>();
 
     constructor(options: { device: Device }) {
         this.device = options.device;
+    }
+
+    target(name: L): Target {
+        return this.map_target.get(name)!;
+    }
+
+    async load_targets(): Promise<void> {
+        logger.debug('load_targets');
+        for (const filename of this.target_list) {
+            const target = new Target(this.device, filename);
+            this.map_target.set(filename, target);
+        }
+        logger.debug('load_targets done');
     }
 
     async wait_for_app_ready(): Promise<void> {
@@ -24,6 +42,10 @@ export abstract class BaseScript {
         logger.debug('wait_for_app_ready done');
     }
 
-    abstract init(): void;
+    abstract init(): Promise<void>;
+
+    abstract play(): Promise<void>;
+
+    abstract finish(): Promise<void>;
 
 }
