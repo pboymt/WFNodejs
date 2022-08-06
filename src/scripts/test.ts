bd024821c541e627cf67467a7351d01f8d3b4985
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-06 17:00:46 +0800

    feat: 添加更多能够应对的意外情况

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -6,7 +6,7 @@ import { BaseScript } from "../core/script";
 const logger = getLogger('test');
 type TargetList = 'ring' | 'btn-ok' | 'btn-ring-join-accept' | 'waiting-room-team-form' |
     'waiting-room-auto-continue-no' | 'waiting-room-ready-no' | 'battle-auto-skill-on' |
-    'finish-alert-team-dismiss' | 'home-btn-chapter' | 'finish-alert-lost-connection';
+    'finish-alert-team-dismiss' | 'home-btn-chapter' | 'finish-alert-lost-connection' | 'buyback';
 
 export class TestScript extends BaseScript<TargetList> {
 
@@ -16,7 +16,7 @@ export class TestScript extends BaseScript<TargetList> {
     target_list = new Set<TargetList>([
         'ring', 'btn-ok', 'btn-ring-join-accept', 'waiting-room-team-form',
         'waiting-room-auto-continue-no', 'waiting-room-ready-no', 'battle-auto-skill-on',
-        'finish-alert-team-dismiss', 'home-btn-chapter', 'finish-alert-lost-connection'
+        'finish-alert-team-dismiss', 'home-btn-chapter', 'finish-alert-lost-connection', 'buyback'
     ]);
 
     constructor(options: { device: Device }) {
@@ -47,7 +47,9 @@ export class TestScript extends BaseScript<TargetList> {
                     break;
                 }
                 // 等待战斗结束
-                await this.wait_for_battle_finish();
+                if (!await this.wait_for_battle_finish()) {
+                    break;
+                }
                 // 等待返回铃铛队伍
                 if (!await this.wait_for_return_to_ring_team()) {
                     break;
@@ -103,15 +105,13 @@ export class TestScript extends BaseScript<TargetList> {
     async wait_for_battle(): Promise<boolean> {
         logger.info('等待战斗');
         while (!await this.target('battle-auto-skill-on').exists()) {
-            if (await this.target('finish-alert-team-dismiss').exists()) {
-                while (await this.target('btn-ok').exists()) {
-                    await this.target('btn-ok').click();
-                    await setTimeout(500);
-                }
+            if (await this.target('btn-ok').exists()) {
+                logger.info('无法返回队伍');
+                await this.target('btn-ok').click();
                 return false;
             }
             if (await this.target('home-btn-chapter').exists()) {
-                logger.info('队伍被解散');
+                logger.info('无法返回队伍');
                 return false;
             }
             await setTimeout(5000);
@@ -120,12 +120,17 @@ export class TestScript extends BaseScript<TargetList> {
         return true;
     }
 
-    async wait_for_battle_finish(): Promise<void> {
+    async wait_for_battle_finish(): Promise<boolean> {
         logger.debug('等待战斗结束');
         while (await this.target('battle-auto-skill-on').exists()) {
+            if (await this.target('buyback').exists()) {
+                logger.info('战斗失败，不再续战');
+                return false;
+            }
             await setTimeout(5000);
         }
         logger.info('战斗结束');
+        return true;
     }
 
     async wait_for_return_to_ring_team(): Promise<boolean> {
@@ -133,7 +138,6 @@ export class TestScript extends BaseScript<TargetList> {
         // TODO 有时候会出现战斗结束后直接返回主页的情况，这时候需要重新等待铃铛
         logger.info('等待返回铃铛队伍');
         while (!await this.target('waiting-room-team-form').exists()) {
-            logger.debug('等待返回铃铛队伍');
             if (await this.target('finish-alert-team-dismiss').exists() || await this.target('finish-alert-lost-connection').exists()) {
                 logger.info('队伍被解散');
                 while (await this.target('btn-ok').exists()) {
