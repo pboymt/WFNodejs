c76498054510d4630fd65dd829f7df14fd3295fd
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-08 23:14:57 +0800

    chore: 引入装饰器和Metadata测试模板预载

--- src/test.ts
+++ src/test.ts
@@ -1,25 +1,67 @@
-import { test } from 'node:test';
 import { createClient } from './core/adb/client';
 import { Device } from './core/adb/device';
 
-(async () => {
+export async function test() {
+    const test_obj = new TestScript();
+    test_obj.test();
+}
 
-    const adb = createClient();
-    const list = await adb.listDevices();
-    const { id, type } = list[0];
-    const device = adb.getDevice(id);
+// decorator with parameters for classes
+function Script(...args: string[]) {
+    return function (constructor: Function) {
+        // console.log('use2', args, constructor);
+    }
+}
 
-    console.log(await device.rotation());
-    console.log(await device.screenSize());
-    console.log(await device.isScreenOn());
-    console.log(await device.appCurrent());
-    // await t.test('Open screen', async (t) => {
-    //     device.shell(['input', 'keyevent', '26']);
-    // });
+let to = {};
 
-    // await t.test('Get screen size', async (t) => {
-    //     const size = await device.screenSize();
-    //     t.is(size.width, 1080);
-    //     t.is(size.height, 1920);
-    // }
-})();
+// decorator with parameters for methods
+function use(...args: string[]) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log(propertyKey, target === to);
+        to = target;
+        const originalMethod = descriptor.value;
+        descriptor.value = function (...oargs: any[]) {
+            console.log(this);
+            console.log(this instanceof TestScript);
+            return originalMethod.apply(this, args);
+        }
+    }
+}
+
+
+@Script()
+class TestScript {
+
+    package_name = 'com.leiting.wf';
+
+    @use('a', 'b')
+    test() {
+        console.log('test');
+    }
+
+    @use('c', 'd')
+    test2() {
+        console.log('test2');
+    }
+
+
+}
+
+@Script()
+class Test2Script {
+
+    package_name = 'com.leiting.wf';
+
+    @use('a', 'b')
+    test() {
+        console.log('test');
+    }
+
+    @use('c', 'd')
+    test2() {
+        console.log('test2');
+    }
+
+
+}
