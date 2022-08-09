31ea8ae1450882665367b57e4a04b90b447a9f75
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-09 16:56:56 +0800

    fix: 修正类型注释错误

--- src/core/script/runner.ts
+++ src/core/script/runner.ts
@@ -1,7 +1,7 @@
 import { Device } from "../adb";
 import { BaseScript } from "./base";
 
-export async function play<T extends { device: Device }>(Script: new (options: T) => BaseScript<any>, options: T): Promise<void> {
+export async function play<T extends { device: Device }>(Script: new (options: T) => BaseScript, options: T): Promise<void> {
     const script = new Script(options);
     await script.load_targets();
     await script.wait_for_app_ready();
