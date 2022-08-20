30d9cfa4b41b29f7d9a468fa5ec54b3bcc283e9c
pboymt <10893582+pboymt@users.noreply.github.com>
2022-08-20 15:20:59 +0800

    perf: 只有一台设备连接在 ADB 时自动选定此设备

--- src/core/adb/choose.ts
+++ src/core/adb/choose.ts
@@ -13,13 +13,17 @@ export async function chooseDevice(id?: string): Promise<Device | undefined> {
             const device = adb.getDevice(choosed_device.id);
             return device;
         } else {
+            if (connected_devices.length === 1) {
+                logger.info(`只有一台设备，自动选择：${connected_devices[0].id}`);
+                return adb.getDevice(connected_devices[0].id);
+            }
             logger.info('没有找到指定设备，请选择一个设备：');
             const { device_id } = await inquirer.prompt([
                 {
                     type: 'list',
                     name: 'device_id',
                     message: '请选择一个设备：',
-                    suffix:'（使用方向键选择，回车键确认）',
+                    suffix: '（使用方向键选择，回车键确认）',
                     choices: connected_devices.map(d => d.id),
                 },
             ]);
